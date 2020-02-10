aee982d09ac1fd45b081dc50839ea67d93ccea79
Eug-VS <eug-vs@keemail.me>
2020-02-10 18:08:52 +0300

    feat: add initial creator.c

---
+++ src/creator.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <windows.h>
+
+int main(int argc, char** argv) {
+  printf("creator: started\n");
+
+  const char* FILENAME = argv[1];
+  const int RECORDS = atoi(argv[2]);
+
+  printf("filename: %s\n", FILENAME);
+  printf("number of records: %d\n", RECORDS);
+
+  printf("creator: finished\n");
+  return 0;
+}
+
