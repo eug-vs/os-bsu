36da676dc6de233a8f190a2a4005abfc01a9a643
Eug-VS <eug-vs@keemail.me>
2020-02-16 01:42:14 +0300

    refactor: improve output in main.c

--- src/main.c
+++ src/main.c
@@ -9,6 +9,7 @@ int main() {
   scanf("%s", filename);
   printf("Enter records number: ");
   scanf("%s", records);
+  printf("\n");
 
   char cmd[128] = "build\\creator.exe ";
   strcat(cmd, filename);
@@ -20,19 +21,19 @@ int main() {
 
   if (CreateProcess(NULL, cmd, NULL, NULL, TRUE, 0, NULL, NULL, &info, &processInfo)) {
     WaitForSingleObject(processInfo.hProcess, INFINITE);
+  } else {
+    printf("Error while creating process!");
+    return 1;
   }
-  else printf("Error while creating process!");
 
   FILE* file = fopen("data", "rb");
+  char c;
   if (file == NULL) return 1;
-  for (int i = 0; i < atoi(records); i++){
-    Employee e;
-    fread(&e, sizeof(Employee), 1, file);
-    printf("Employee #%d\n", e.num);
-    printf("Name: %s\n", e.name);
-    printf("Hours: %lf\n", e.hours);
-    printf("\n");
-  }
+  printf("Binary file saved to ./%s with the following contents:\n", filename);
+  do {
+    c = fgetc(file);
+    printf("%c", c);
+  } while (c != EOF);
   fclose(file);
 
   return 0;
