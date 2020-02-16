97c5a9a03cefcba5d7572b74ee29497edf5f8727
Eug-VS <eug-vs@keemail.me>
2020-02-16 03:17:13 +0300

    feat: implement full reporter functionality

--- src/reporter.c
+++ src/reporter.c
@@ -1,6 +1,7 @@
 #include "header.h"
 
-#define HEADLINE_TEMPLATE "Report for file %s\n"
+#define HEADLINE_TEMPLATE "Report for file: %s \n\n%-10s%-10s%-10s%-10s\n"
+#define ROW_TEMPLATE "%-10d%-10s%-10g%-10g\n"
 
 
 int main(int argc, char** argv) {
@@ -8,7 +9,12 @@ int main(int argc, char** argv) {
   FILE* report = fopen(argv[2], "w+");
   double hour_payment = strtod(argv[3], NULL);
 
-  fprintf(report, HEADLINE_TEMPLATE, argv[1]);
+  fprintf(report, HEADLINE_TEMPLATE, argv[1], "Id", "Name", "Hours", "Salary");
+
+  Employee e;
+  while(fread(&e, sizeof(Employee), 1, file)) {
+    fprintf(report, ROW_TEMPLATE, e.num, e.name, e.hours, e.hours * hour_payment);
+  };
 
   fclose(file);
   fclose(report);
