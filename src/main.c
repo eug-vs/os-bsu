71e2b1ce1d34c9c816f54aa59f613f5cf7defd4c
eug-vs <eug-vs@keemail.me>
2020-03-14 04:12:12 +0300

    fix: use paths relative to data folder

--- src/main.c
+++ src/main.c
@@ -2,25 +2,27 @@
 #include "await.h"
 #include "cat.h"
 
+#define DATA_DIR_PREFIX "data\\"
+
 int main() {
   char cmd[256];
-  char binary[64];
-  char report[64];
-  char records[8];
+  char binary[64] = DATA_DIR_PREFIX;
+  char report[64] = DATA_DIR_PREFIX;
+  int records;
   double payment;
 
   printf("Enter binary filename: ");
-  scanf("%s", binary);
+  scanf("%s", binary + 5);
   printf("Enter records number: ");
-  scanf("%s", records);
+  scanf("%i", &records);
   puts("");
-  sprintf(cmd, "bin\\creator.exe %s %s", binary, records);
+  sprintf(cmd, "bin\\creator.exe %s %i", binary, records);
   await(cmd);
   printf("Binary file saved to \"%s\" with the following contents:\n", binary);
   cat(binary, 1);
 
   printf("Enter report filename: ");
-  scanf("%s", report);
+  scanf("%s", report + 5);
   printf("Enter payment amount per hour: ");
   scanf("%lf", &payment);
   puts("");
